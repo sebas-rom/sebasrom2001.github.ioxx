e94eaa7741980ee053dd6230f41f4ef16927363a
sebasrom2001 <gabrielsebastian.romero@gmail.com>
2023-06-18 21:07:08 -0500

    Commit

---
+++ JSON-translation-data.js
@@ -0,0 +1,24 @@
+var linguJSON = {
+    "languages": [
+      {
+          "lang_name": "English",
+          "lang_code": "en",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "Español",
+          "lang_code": "es",
+          "url_pattern": "?"
+      }
+    ],
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a></div>",
+    "custom_lang_switcher_container_id": "menuContainer",
+    "translated_segments": [
+        {
+            "source": "Hi! I'm Sebastian Romero",
+            "target_es": "Hola Soy Sebastián Romero"
+        },
+        
+    ]
+};
+                                                 
