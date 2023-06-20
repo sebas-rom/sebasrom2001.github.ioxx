cf9960312d3bb841b26af33eb0d6c5dad95bd237
sebasrom2001 <gabrielsebastian.romero@gmail.com>
2023-06-20 10:54:09 -0500

    NavUpdate

--- JSON-translation-data.js
+++ JSON-translation-data.js
@@ -11,7 +11,7 @@ var linguJSON = {
           "url_pattern": "?"
       }
     ],
-    "custom_lang_switcher_html": "<div class=\"dropdown\"><button class=\"dropbtn\"><i class=\'fa fa-globe\' style=\'font-size:36px;color:#000000\'></i></button><div id=\"langswitcher\" class=\"notranslate dropdown-content\"><a href=\"?lang=en\">Eng</a> <a href=\"?lang=es\" >Esp</a></div>",
+    "custom_lang_switcher_html": "<div class=\"dropdown\"><button class=\"dropbtn\"><i class=\'fa fa-globe\'></i></button><div id=\"langswitcher\" class=\"notranslate dropdown-content\"><a href=\"?lang=en\">Eng</a> <a href=\"?lang=es\" >Esp</a></div>",
     "custom_lang_switcher_container_id": "langlinks",
     "translated_segments": [
         {
--- functions.js
+++ functions.js
@@ -1,22 +1,10 @@
 
 function showNavDropdown() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className == "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
 }
 
-window.onscroll = function() {stickyNavBar()};
-
-var navbar = document.getElementById("myTopnav");
-var sticky = navbar.offsetTop;
-
-function stickyNavBar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
