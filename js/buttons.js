7db145fd73ba298d9958f9c26ffb744955b1d372
Sebastián Romero <57327474+sebasrom2001@users.noreply.github.com>
2023-07-15 18:54:46 -0500

    Delete js directory

--- js/buttons.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("show-more-services");
-
-button.addEventListener("click", function() {
-  if (button.textContent === "SHOW MORE") {
-    button.textContent = "SHOW LESS";
-  } else {
-    button.textContent = "SHOW MORE";
-  }
-});
